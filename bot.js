fe86f1584ac7210088dbddf42193ceaa0b6408c1
hhoo1645 <43479514+hhoo1645@users.noreply.github.com>
2019-06-08 22:53:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "-";
 var adminprefix = '-'
 
 
-const developers = ["518502376680849409","407244267984125972"]
+const developers = ["579979579624521750","579979579624521750"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
